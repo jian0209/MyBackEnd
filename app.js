89fa97014d583f3b2d1f74ce463d7fae2ff55f3e
tey kai jian <jian0209@teys-MacBook-Air.local>
2022-07-08 14:04:16 +0800

    heroku testing

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+var cors = require('cors')
 
 var indexRouter = require('./routes/index')
-// var usersRouter = require('./routes/users');
 
 var app = express()
 
@@ -13,6 +13,7 @@ var app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -37,56 +38,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500)
   res.render('error')
 })
-
-var isValid = function (s) {
-  if (s.length < 1) {
-    return false
-  }
-  let arr = s.split('')
-  let returnArr = []
-  arr.forEach((item) => {
-    console.log(item)
-    switch (item) {
-      case '(':
-        returnArr.push('(')
-        break
-      case '{':
-        returnArr.push('{')
-        break
-      case '[':
-        returnArr.push('[')
-        break
-      case ')':
-        if (returnArr.indexOf('(') !== -1) {
-          returnArr.splice(returnArr.indexOf('('), 1)
-        } else {
-          returnArr.push(')')
-        }
-        break
-      case '}':
-        if (returnArr.indexOf('{') !== -1) {
-          returnArr.splice(returnArr.indexOf('{'), 1)
-        } else {
-          returnArr.push('}')
-        }
-        break
-      case ']':
-        if (returnArr.indexOf('[') !== -1) {
-          returnArr.splice(returnArr.indexOf('['), 1)
-        } else {
-          returnArr.push(']')
-        }
-        break
-    }
-  })
-  console.log(returnArr)
-  if (returnArr.length < 1) {
-    return true
-  } else {
-    return false
-  }
-}
-
-console.log(isValid('([)]'))
 
 module.exports = app
